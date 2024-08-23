9800f3e0c4ff9b3b27bc188a7fd213877b7e430f
Ralitsa Dimitrova <ralitsa.n.dimitrova@gmail.com>
2024-08-23 17:41:39 +0300

    [Simple R] Solutions to section 2

---
+++ section_2.r
@@ -0,0 +1,12 @@
+# Simple R
+# Section 2 - Data
+# Problem 1
+miles = c(65311, 65624, 65908, 66219, 66499, 66821, 67145, 67447)
+x = diff(miles)
+max(miles)
+min(miles)
+
+# Problem 2
+commutes = c(17, 16, 20, 24, 22, 15, 21, 15, 17, 22)
+commutes[commutes==24]=18
+
