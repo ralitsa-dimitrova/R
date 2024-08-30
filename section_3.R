503bdf1846feeb62dd231a276f64b8882e8c1c26
Ralitsa Dimitrova <ralitsa.n.dimitrova@gmail.com>
2024-08-30 17:07:35 +0300

    [Simple R] Solutions to problems 3.2-3.3

--- section_3.R
+++ section_3.R
@@ -4,3 +4,15 @@
 # Problem 3.1
 x = c(60, 85, 72, 59, 37, 75, 93, 7, 98, 63, 41, 90, 5, 17, 97)
 stem(x)
+
+
+# Problem 3.2
+x= c(80, 82, 88, 91, 91, 95, 95, 97, 98, 101, 106, 106, 109, 110, 111)
+stem(x) # check if my interpretation is correct
+hist(x)
+
+# Problem 3.3
+x = rnorm(100)
+hist(x) # different results on each definition of x
+
+
