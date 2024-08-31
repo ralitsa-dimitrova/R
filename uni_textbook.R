d7e36fec1102f86ed5b18c5c722b6a368d13da11
Ralitsa Dimitrova <ralitsa.n.dimitrova@gmail.com>
2024-08-31 16:33:30 +0300

    [Uni textbook] Solutions to 5.1-5.5 + 12.1-12.2

---
+++ uni_textbook.R
@@ -0,0 +1,149 @@
+# Problems from university notes
+
+N = 1000 # Total number of samples
+
+# Problem 5.1
+
+# There is box with 5 balls inside it, numbered 1 through 5.
+# You retrieve a ball from the box, return it and then retrieve a ball again.
+# What is the probability to retrieve that you will take same ball twice?
+
+# Solution
+
+balls.test = function(N) {
+  ball.samples = sapply(1:N, function (x) sample(1:5, 2, replace=TRUE))
+  sum(ball.samples[1,] == ball.samples[2,])/N
+}
+
+# Run
+
+balls.test(1000)
+balls.test(10000)
+balls.test(100000)
+
+# Problem 5.2
+
+# Throwing a dice. 
+# Is the relative frequency of throwing a 6 close to the theoretical probability of 1/6?
+
+# Solution
+
+dice.test = function(N) {
+  dice.samples = sample(1:6, N, replace = TRUE)
+  table(dice.samples)[6]/N
+}
+
+dice.test(10000)
+
+# Problem 5.3
+# What is the probability that out of a group of 20 people, 
+# there will be at least two people who share a birthday?
+
+bdays.data = sample(1:365, 20, T) # Generate 1 sample
+N = 100 # Total samples
+bdays.table = sapply(1:N, function (x) sample(1:365, 20, T)) # Generate N samples
+table(bdays.table[,1]) # Group b-day dates by people
+max(table(bdays.table[,1])) # Get the count of the people which have a b-day on the most common b-day date
+bdays.mostcommondates = apply(bdays.table, 2, function(x) max(table(x)))
+sum(bdays.mostcommondates>=2)
+
+bday.problem = function(N) {
+  bday.samples = sapply(1:N, function (x) sample(1:365, 20, T))
+  bday.peopleByDateCount = apply(bday.samples, 2, function(x) max(table(x)))
+  sum(bday.peopleByDateCount>=2)/N
+}
+
+bday.problem(10000)
+
+# Problem 5.4
+# In the course of a week you receive a total of 12 phone calls.
+# What is the probability that you will get at least one phone call on each
+# day of the week?
+
+
+calls = sample(1:7, 12, T) # Generate 1 sample
+calls.table = sapply(1:N, function(x) sample(1:7, 12, T), T)
+
+
+# Solution B - Without a for cycle
+
+count.weekdays = function(calls.sample) {
+  b = vector("numeric", 7)
+  for (i in 1:length(calls.sample)) {
+    b[calls.sample[i]] = 1
+  }
+  sum(b)
+}
+
+calls.test = function(N) {
+  calls.samples = sapply(1:N, function(X) sample(1:7, 12, replace = TRUE))
+  calls.weekdays = apply(calls.samples, 2, function(x) count.weekdays(x))
+  sum(calls.weekdays==7)/N
+}
+
+calls.test(1000)
+
+
+calls = function(N) {
+  calls.table = sapply(1:N, function(x) sample(1:7, 12, T), T)
+  for (i in 1:N) {
+    
+  }
+  days.called
+}
+
+# Solution B - With a for cycle
+
+calls2 = function(N) {
+  res = 0
+  for(i in 1:N) {
+    calls.sample = sample(1:7, 12, replace=T)
+    if (count.weekdays(calls.sample)==7) {
+      res=res+1
+    }
+  }
+  res/N
+}
+
+calls2(1000)
+
+# Problem 5.5
+
+# There are 3 playing cards
+# - 1) Both sides are white
+# - 2) Both sides are black
+# - 3) One side is white, the other - black
+# We choose a random card. The side which is facing up is white.
+# What is the probability that the other side is also white?
+
+gen.sample = function() {
+  card = sample(c("bb", "bw", "ww"), 1)
+  side = sample(1:2, 1)
+  up = substr(card, side, side)
+  c(card, up)
+}
+
+cards.test = function(N) {
+  samples = sapply(1:N, function(x) gen.sample())
+  sum(samples[1,] == "ww") / sum(samples[2,] == "w")
+}
+
+# Problem 12.1
+
+# John has 5 keys, but isn't sure which one is for his room.
+# He tries each of the keys, remembering the ones he checked.
+# What is the probability that he unlocks the room on the 5th key?
+
+keys.test = function(N) {
+  keys.samples = sapply(1:N, function(x) sample(1:5, 5, replace=FALSE))
+  sum(keys.samples[5,] == 5)/N
+}
+
+keys.test(10000)
+
+# Problem 12.2
+
+# There is a deck of shuffled cards.
+# The cards get distributed among 4 players.
+# What is the probability that each player has one ace?
+
